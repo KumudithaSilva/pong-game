36fbcbcdbd5d342f45fd15d20b87682eb0b6d0c6
kumuditha <kumudithasilva66@gmail.com.>
2025-07-10 19:43:39 +0530

    Add ball moving and sleep screen

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from ball import Ball
 from paddle import Paddle
+import time
 
-# TODO 3: Create another paddle
-# TODO 4: Create the ball and make it move
 # TODO 5: Detect collision with wall and bounce
 # TODO 6: Detect collision with paddle misses
 # TODO 7: Keep score
@@ -15,8 +15,12 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 # TODO 2: Create and move a paddle
-paddle_left = Paddle(-350, 0)
-paddle_right = Paddle(350, 0)
+# TODO 3: Create another paddle
+paddle_left = Paddle((-350, 0))
+paddle_right = Paddle((350, 0))
+
+# TODO 4: Create the ball and make it move
+ball = Ball()
 
 screen.listen()
 screen.onkey(paddle_right.move_up, "Up")
@@ -25,11 +29,12 @@ screen.onkey(paddle_right.move_down, "Down")
 screen.onkey(paddle_left.move_up, "w")
 screen.onkey(paddle_left.move_down, "s")
 
+# TODO 4: Create the ball and make it move
 game_is_on = True
 while game_is_on:
+    time.sleep(0.05)
+    ball.move()
     screen.update()
-
-
 
 
 
