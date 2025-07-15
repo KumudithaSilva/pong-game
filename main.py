7c0107b80887a2e5ae0680f25261cbc384a06d12
kumuditha <kumudithasilva66@gmail.com.>
2025-07-15 22:52:24 +0530

    Register the images and initialize the GameManager, BoundaryNet

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from ball import Ball
+from boundarynet import BoundaryNet
+from gamemanager import GameManager
 from paddle import Paddle
 from  scoreboard import  Scoreboard
 import time
 
+WINNING_SCORE = 5
+
 # TODO 1: Create the screen
 screen = Screen()
-scoreboard = Scoreboard()
+screen.register_shape("left_paddle.gif")
+screen.register_shape("right_paddle.gif")
+screen.register_shape("pong_ball.gif")
+screen.bgpic("pong_background.gif")
+
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
-screen.bgcolor("black")
 screen.tracer(0)
+
+boundary_net = BoundaryNet()
+scoreboard = Scoreboard()
 
 # TODO 2: Create and move a paddle
 # TODO 3: Create another paddle
 paddle_left = Paddle((-350, 0))
 paddle_right = Paddle((350, 0))
 
+paddle_left.shape("left_paddle.gif")
+paddle_right.shape("right_paddle.gif")
+
 # TODO 4: Create the ball and make it move
 ball = Ball()
+ball.shape("pong_ball.gif")
 
 screen.listen()
 screen.onkey(paddle_right.move_up, "Up")
@@ -26,6 +40,9 @@ screen.onkey(paddle_right.move_down, "Down")
 
 screen.onkey(paddle_left.move_up, "w")
 screen.onkey(paddle_left.move_down, "s")
+
+manager = GameManager(ball, boundary_net, paddle_left, paddle_right, scoreboard)
+
 
 # TODO 4: Create the ball and make it move
 game_is_on = True
@@ -40,8 +57,13 @@ while game_is_on:
             or (ball.distance(paddle_left) < 50 and ball.xcor() < -320)):
         ball.bounce_x()
 
-    if scoreboard.l_score >= 10 or scoreboard.r_score >= 10:
+    if scoreboard.l_score >= WINNING_SCORE or scoreboard.r_score >= WINNING_SCORE:
         game_is_on = False
+        if scoreboard.l_score == scoreboard.r_score:
+            winner = "It's a Draw"
+        else:
+            winner = "A" if scoreboard.l_score > scoreboard.r_score else "B"
+        manager.game_over(winner)
 
     # TODO 6: Detect collision with paddle misses
     # TODO 7: Keep score
