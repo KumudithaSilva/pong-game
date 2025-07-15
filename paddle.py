6b98837d223ad921fc93b56db1678a9d6abfebda
kumuditha <kumudithasilva66@gmail.com.>
2025-07-15 22:50:33 +0530

    Remove the shape and color from the initial paddles

--- paddle.py
+++ paddle.py
@@ -3,8 +3,6 @@ from turtle import Screen, Turtle
 class Paddle(Turtle):
     def __init__(self, position):
         super().__init__()
-        self.shape("square")
-        self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
